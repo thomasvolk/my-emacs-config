22a255a82d6fcab86589e23aa13d726d3daa518e
Thomas Volk (MacBook) <info@thomasvolk.de>
2025-09-08 19:12:08 +0200

    add key bindings for navigation

--- vanilla/key-bindings.el
+++ vanilla/key-bindings.el
@@ -8,4 +8,11 @@
 ;; evil bindings
 (evil-set-leader 'motion (kbd "SPC"))
 (evil-define-key 'normal 'global (kbd "<leader>n n") 'treemacs-select-window)
+(evil-define-key 'normal 'global (kbd "<leader>n t") 'treemacs)
+(evil-define-key 'normal 'global (kbd "<leader>n p") 'treemacs-display-current-project-exclusively)
 (evil-define-key 'normal 'global (kbd "<leader>b b") 'ibuffer)
+(evil-define-key 'normal 'global (kbd "<leader>x x") 'shell-command)
+(evil-define-key 'normal 'global (kbd "<leader>w <left>") 'windmove-left)
+(evil-define-key 'normal 'global (kbd "<leader>w <right>") 'windmove-right)
+(evil-define-key 'normal 'global (kbd "<leader>w <up>") 'windmove-up)
+(evil-define-key 'normal 'global (kbd "<leader>w <down>") 'windmove-down)
