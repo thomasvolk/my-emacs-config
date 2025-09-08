620138b8a96a69d568bc76878a8aa5d0dc08a2c8
Thomas Volk (MacBook) <info@thomasvolk.de>
2025-09-08 20:34:10 +0200

    add key bindings

--- vanilla/key-bindings.el
+++ vanilla/key-bindings.el
@@ -7,12 +7,21 @@
 
 ;; evil bindings
 (evil-set-leader 'motion (kbd "SPC"))
+;; treemacs
 (evil-define-key 'normal 'global (kbd "<leader>n n") 'treemacs-select-window)
 (evil-define-key 'normal 'global (kbd "<leader>n t") 'treemacs)
 (evil-define-key 'normal 'global (kbd "<leader>n p") 'treemacs-display-current-project-exclusively)
+;; buffer
 (evil-define-key 'normal 'global (kbd "<leader>b b") 'ibuffer)
+(evil-define-key 'normal 'global (kbd "<leader>b m") 'buffer-menu-open)
+(evil-define-key 'normal 'global (kbd "<leader>b <left>") 'previous-buffer)
+(evil-define-key 'normal 'global (kbd "<leader>b <right>") 'next-buffer)
+;; shell
 (evil-define-key 'normal 'global (kbd "<leader>x x") 'shell-command)
+;; windows
 (evil-define-key 'normal 'global (kbd "<leader>w <left>") 'windmove-left)
 (evil-define-key 'normal 'global (kbd "<leader>w <right>") 'windmove-right)
 (evil-define-key 'normal 'global (kbd "<leader>w <up>") 'windmove-up)
 (evil-define-key 'normal 'global (kbd "<leader>w <down>") 'windmove-down)
+;; projects
+(evil-define-key 'normal 'global (kbd "<leader>p p") 'projectile-switch-project)
