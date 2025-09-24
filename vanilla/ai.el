289e3d3a740f5c6257bd07984e0c5ba3c5ac62f8
Thomas Volk (MBP-von-Thomas.fritz.box) <info@thomasvolk.de>
2025-09-24 19:44:12 +0200

    remove codeium

--- vanilla/ai.el
+++ vanilla/ai.el
@@ -11,19 +11,3 @@
   )
   nil
 )
-
-(if (string= "codeium" (getenv "EDITOR_AI"))
-    (use-package codeium
-      :vc (:url "https://github.com/Exafunction/codeium.el"
-            :rev :newest
-            :branch "main")
-      :init
-      (add-to-list 'completion-at-point-functions #'codeium-completion-at-point)
-      :config
-      (setq use-dialog-box nil)
-      ;; Add Codeium to company backends
-      (add-to-list 'company-backends 'codeium-company)
-      ;; Run M-x codeium-install after installation
-    )
-    nil
-)
