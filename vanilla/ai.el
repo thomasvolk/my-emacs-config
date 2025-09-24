8e8918ff366442dd9fa6ea3ae3c03a2057734506
Thomas Volk <thomas.volk@trustedshops.de>
2025-09-24 15:07:29 +0200

    codeium integration

--- vanilla/ai.el
+++ vanilla/ai.el
@@ -9,5 +9,17 @@
     (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
     (global-set-key (kbd "C-c a") 'copilot-mode)
   )
-  nil
-)
+  (
+    (use-package codeium
+      :vc (:url "https://github.com/Exafunction/codeium.el"
+            :rev :newest
+            :branch "main")
+      :init
+      (add-to-list 'completion-at-point-functions #'codeium-completion-at-point)
+      :config
+      (setq use-dialog-box nil)
+      ;; Add Codeium to company backends
+      (add-to-list 'company-backends 'codeium-company)
+      ;; Run M-x codeium-install after installation
+      )
+    )
